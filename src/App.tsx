ede9df9e1f214a4d52f4812881fc7a225ff16bf0
Joshtri <stuffofyos1516@gmail.com>
2025-01-29 16:04:36 +0800

    Refactor App component to integrate QueryClient, update routing, and enhance resource management

--- src/App.tsx
+++ src/App.tsx
@@ -1,120 +1,170 @@
-import { GitHubBanner, Refine } from "@refinedev/core";
-import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import React from "react";  
+import { GitHubBanner, Refine } from "@refinedev/core";  
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";  
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";  
+import {  
+  ErrorComponent,  
+  RefineSnackbarProvider,  
+  ThemedLayoutV2,  
+  useNotificationProvider,  
+} from "@refinedev/mui";  
+import CssBaseline from "@mui/material/CssBaseline";  
+import GlobalStyles from "@mui/material/GlobalStyles";  
+import routerBindings, {  
+  DocumentTitleHandler,  
+  NavigateToResource,  
+  UnsavedChangesNotifier,  
+} from "@refinedev/react-router";  
+import dataProvider from "@refinedev/simple-rest";  
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";  
+import { Header } from "./components/header";  
+import { ColorModeContextProvider } from "./contexts/color-mode";  
+import { JemaatProvider } from "./providers/jemaat-data-provider"; // Import your JemaatProvider  
+import { JemaatCreate, JemaatEdit, JemaatList, JemaatShow } from "./pages/jemaats";  
+import { KartuKeluargaCreate, KartuKeluargaEdit, KartuKeluargaList, KartuKeluargaShow } from "./pages/kartu-keluargas";  
 
-import {
-  ErrorComponent,
-  RefineSnackbarProvider,
-  ThemedLayoutV2,
-  useNotificationProvider,
-} from "@refinedev/mui";
+import { authProvider } from "./providers/auth-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // Import QueryClient and QueryClientProvider  
+import { MuiInferencer } from "@refinedev/inferencer/mui";
+import { LoginPage } from "./pages/login";
+import { DashboardPage } from "./pages/dashboard";
+import { Title } from "./components/title";
 
-import CssBaseline from "@mui/material/CssBaseline";
-import GlobalStyles from "@mui/material/GlobalStyles";
-import routerBindings, {
-  DocumentTitleHandler,
-  NavigateToResource,
-  UnsavedChangesNotifier,
-} from "@refinedev/react-router";
-import dataProvider from "@refinedev/simple-rest";
-import { BrowserRouter, Outlet, Route, Routes } from "react-router";
-import { Header } from "./components/header";
-import { ColorModeContextProvider } from "./contexts/color-mode";
-import {
-  BlogPostCreate,
-  BlogPostEdit,
-  BlogPostList,
-  BlogPostShow,
-} from "./pages/blog-posts";
-import {
-  CategoryCreate,
-  CategoryEdit,
-  CategoryList,
-  CategoryShow,
-} from "./pages/categories";
+import "@fontsource/poppins/400.css"; // Regular weight
+import "@fontsource/poppins/600.css"; // Semi-bold weight
+import { Dashboard, EmojiPeople, FamilyRestroom, Map } from "@mui/icons-material";
+import { RayonCreate, RayonEdit, RayonList, RayonShow } from "./pages/rayons";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <GitHubBanner />
-      <RefineKbarProvider>
-        <ColorModeContextProvider>
-          <CssBaseline />
-          <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
-          <RefineSnackbarProvider>
-            <DevtoolsProvider>
-              <Refine
-                dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-                notificationProvider={useNotificationProvider}
-                routerProvider={routerBindings}
-                resources={[
-                  {
-                    name: "blog_posts",
-                    list: "/blog-posts",
-                    create: "/blog-posts/create",
-                    edit: "/blog-posts/edit/:id",
-                    show: "/blog-posts/show/:id",
-                    meta: {
-                      canDelete: true,
-                    },
-                  },
-                  {
-                    name: "categories",
-                    list: "/categories",
-                    create: "/categories/create",
-                    edit: "/categories/edit/:id",
-                    show: "/categories/show/:id",
-                    meta: {
-                      canDelete: true,
-                    },
-                  },
-                ]}
-                options={{
-                  syncWithLocation: true,
-                  warnWhenUnsavedChanges: true,
-                  useNewQueryKeys: true,
-                  projectId: "sxqIfL-WcNGSc-e6m1Sh",
-                }}
-              >
-                <Routes>
-                  <Route
-                    element={
-                      <ThemedLayoutV2 Header={() => <Header sticky />}>
-                        <Outlet />
-                      </ThemedLayoutV2>
-                    }
-                  >
-                    <Route
-                      index
-                      element={<NavigateToResource resource="blog_posts" />}
-                    />
-                    <Route path="/blog-posts">
-                      <Route index element={<BlogPostList />} />
-                      <Route path="create" element={<BlogPostCreate />} />
-                      <Route path="edit/:id" element={<BlogPostEdit />} />
-                      <Route path="show/:id" element={<BlogPostShow />} />
-                    </Route>
-                    <Route path="/categories">
-                      <Route index element={<CategoryList />} />
-                      <Route path="create" element={<CategoryCreate />} />
-                      <Route path="edit/:id" element={<CategoryEdit />} />
-                      <Route path="show/:id" element={<CategoryShow />} />
-                    </Route>
-                    <Route path="*" element={<ErrorComponent />} />
-                  </Route>
-                </Routes>
 
-                <RefineKbar />
-                <UnsavedChangesNotifier />
-                <DocumentTitleHandler />
-              </Refine>
-              <DevtoolsPanel />
-            </DevtoolsProvider>
-          </RefineSnackbarProvider>
-        </ColorModeContextProvider>
-      </RefineKbarProvider>
-    </BrowserRouter>
-  );
-}
+const queryClient = new QueryClient(); // Create a QueryClient instance  
+  
+function App() {  
+  return (  
+    <BrowserRouter>  
+      <QueryClientProvider client={queryClient}> {/* Wrap with QueryClientProvider */}  
+        <RefineKbarProvider>  
+          <ColorModeContextProvider>  
+            <JemaatProvider>  
+              <CssBaseline />  
+              <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />  
+              <RefineSnackbarProvider>  
+                <DevtoolsProvider>  
+                  <Refine
+                    authProvider={authProvider}
+                    dataProvider={dataProvider(`${import.meta.env.VITE_BASE_URL}/api`)}  
+                    notificationProvider={useNotificationProvider}  
+                    routerProvider={routerBindings}  
+                    LoginPage={LoginPage} // Ini akan menampilkan halaman login saat user belum terautentikasi
+                    resources={[
+                      {
+                        name: "dashboard",
+                        list: "/dashboard",
+                        meta: {
+                          label: "Dashboard",
+                          icon: <Dashboard />,
+                        },
+                      },
+                      {  
+                        name: "jemaat",  
+                        list: "/jemaat",  
+                        create: "/jemaat/create",  
+                        edit: "/jemaat/edit/:id",  
+                        show: "/jemaat/show/:id",
+                        meta: {
+                          label: "Jemaat",
+                          icon: <EmojiPeople/>
+                        },
+                        canDelete: true,
 
-export default App;
+
+                      },  
+                      {  
+                        name: "kartu-keluarga",  
+                        list: "/kartu-keluarga",  
+                        create: "/kartu-keluarga/create",  
+                        edit: "/kartu-keluarga/edit/:id",  
+                        show: "/kartu-keluarga/show/:id",  
+                        meta: {
+                          label: "Kartu Keluarga",
+                          icon: <FamilyRestroom/>
+                        },
+                        canDelete: true,
+                      },  
+                      {  
+                        name: "rayon",  
+                        list: "/rayon",  
+                        create: "/rayon/create",  
+                        edit: "/rayon/edit/:id",  
+                        show: "/rayon/show/:id",
+                        meta: {
+                          label: "Rayon",
+                          icon: <Map/>
+                        },
+                        canDelete: true,
+
+                      }  
+                    ]}  
+                    options={{  
+                      syncWithLocation: true,  
+                      warnWhenUnsavedChanges: true,  
+                      useNewQueryKeys: true,  
+                      projectId: "sxqIfL-WcNGSc-e6m1Sh",  
+                    }}  
+                  >  
+                    <Routes>
+                      {/* Tambahkan route untuk login */}
+                      <Route path="/login" element={<LoginPage />} />
+                      <Route  
+                        element={  
+                          <ThemedLayoutV2 Header={() => <Header sticky /> } Title={Title}>  
+                            <Outlet />  
+                          </ThemedLayoutV2>  
+                        }>  
+                        <Route  
+                          index  
+                          element={<NavigateToResource resource="jemaat" />}  
+                        />
+
+                        <Route path='/dashboard' element={<DashboardPage />} />
+
+                        <Route path="/jemaat">  
+                          <Route index element={<JemaatList />} />  
+                          <Route path="create" element={<JemaatCreate />} />  
+                          <Route path="edit/:id" element={<JemaatEdit />} />  
+                          <Route path="show/:id" element={<JemaatShow />} />  
+                        </Route>  
+  
+                        <Route path="/kartu-keluarga">  
+                          <Route index element={<KartuKeluargaList />} />  
+                          <Route path="create" element={<KartuKeluargaCreate />} />  
+                          <Route path="edit/:id" element={<KartuKeluargaEdit />} />  
+                          <Route path="show/:id" element={<KartuKeluargaShow />} />  
+                        </Route>
+
+                        <Route path="/rayon">
+                          <Route index element={<RayonList />} />
+                          <Route path='create' element={<RayonCreate/>} />
+                          <Route path='edit/:id' element={<RayonEdit/>} />
+                          <Route path='show/:id' element={<RayonShow/>} />
+                        </Route>
+  
+                        <Route path="*" element={<ErrorComponent />} />  
+                      </Route>  
+                    </Routes>  
+  
+                    <RefineKbar />  
+                    <UnsavedChangesNotifier />  
+                    <DocumentTitleHandler />  
+                  </Refine>  
+                  <DevtoolsPanel />  
+                </DevtoolsProvider>  
+              </RefineSnackbarProvider>  
+            </JemaatProvider>  
+          </ColorModeContextProvider>  
+        </RefineKbarProvider>  
+      </QueryClientProvider> {/* Close QueryClientProvider */}  
+    </BrowserRouter>  
+  );  
+}  
+  
+export default App;  
