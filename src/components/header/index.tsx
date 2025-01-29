bb76e40a9efdf81115eb3800e2bb44e10bfceb76
Joshtri <stuffofyos1516@gmail.com>
2025-01-29 16:05:26 +0800

    Enhance Header component with profile menu and toggle for dark/light mode

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,43 +3,48 @@ import LightModeOutlined from "@mui/icons-material/LightModeOutlined";
 import AppBar from "@mui/material/AppBar";
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 import Stack from "@mui/material/Stack";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { useGetIdentity } from "@refinedev/core";
-import { HamburgerMenu, RefineThemedLayoutV2HeaderProps } from "@refinedev/mui";
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
+import { HamburgerMenu } from "@refinedev/mui";
 import { ColorModeContext } from "../../contexts/color-mode";
 
-type IUser = {
-  id: number;
-  name: string;
-  avatar: string;
-};
-
-export const Header: React.FC<RefineThemedLayoutV2HeaderProps> = ({
-  sticky = true,
-}) => {
+export const Header: React.FC = () => {
   const { mode, setMode } = useContext(ColorModeContext);
 
-  const { data: user } = useGetIdentity<IUser>();
+  const [profileAnchorEl, setProfileAnchorEl] =
+    useState<null | HTMLElement>(null);
+
+  const handleProfileMenuClick = (event: React.MouseEvent<HTMLElement>) => {
+    setProfileAnchorEl(event.currentTarget);
+  };
+
+  const handleProfileMenuClose = () => {
+    setProfileAnchorEl(null);
+  };
 
   return (
-    <AppBar position={sticky ? "sticky" : "relative"}>
+    <AppBar position="sticky">
       <Toolbar>
+      <HamburgerMenu/>
+
         <Stack
           direction="row"
           width="100%"
           justifyContent="flex-end"
           alignItems="center"
         >
-          <HamburgerMenu />
           <Stack
             direction="row"
-            width="100%"
             justifyContent="flex-end"
             alignItems="center"
+            gap={2}
           >
+            
+            {/* Tombol Toggle Dark/Light Mode */}
             <IconButton
               color="inherit"
               onClick={() => {
@@ -49,29 +54,34 @@ export const Header: React.FC<RefineThemedLayoutV2HeaderProps> = ({
               {mode === "dark" ? <LightModeOutlined /> : <DarkModeOutlined />}
             </IconButton>
 
-            {(user?.avatar || user?.name) && (
-              <Stack
-                direction="row"
-                gap="16px"
-                alignItems="center"
-                justifyContent="center"
+            {/* Informasi Profil Pengguna */}
+            <Stack direction="row" gap="16px" alignItems="center">
+              <Typography
+                sx={{
+                  display: {
+                    xs: "none",
+                    sm: "inline-block",
+                  },
+                }}
+                variant="subtitle2"
               >
-                {user?.name && (
-                  <Typography
-                    sx={{
-                      display: {
-                        xs: "none",
-                        sm: "inline-block",
-                      },
-                    }}
-                    variant="subtitle2"
-                  >
-                    {user?.name}
-                  </Typography>
-                )}
-                <Avatar src={user?.avatar} alt={user?.name} />
-              </Stack>
-            )}
+                John Doe
+              </Typography>
+              <IconButton onClick={handleProfileMenuClick}>
+                <Avatar
+                  src="https://via.placeholder.com/40"
+                  alt="John Doe"
+                />
+              </IconButton>
+              <Menu
+                anchorEl={profileAnchorEl}
+                open={Boolean(profileAnchorEl)}
+                onClose={handleProfileMenuClose}
+              >
+                <MenuItem onClick={handleProfileMenuClose}>Profile</MenuItem>
+                <MenuItem onClick={handleProfileMenuClose}>Logout</MenuItem>
+              </Menu>
+            </Stack>
           </Stack>
         </Stack>
       </Toolbar>
