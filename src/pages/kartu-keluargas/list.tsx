11d40c79bac8a793e2aa43be790caa81438259ad
Joshtri <stuffofyos1516@gmail.com>
2025-01-29 15:57:22 +0800

    Add KartuKeluargaList component for displaying kartu keluarga data with server-side pagination and actions

---
+++ src/pages/kartu-keluargas/list.tsx
@@ -0,0 +1,83 @@
+import { MuiInferencer } from "@refinedev/inferencer/mui";
+import {
+    useDataGrid,
+    EditButton,
+    ShowButton,
+    DeleteButton,
+    List,
+  } from "@refinedev/mui";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { Box, Typography, LinearProgress } from "@mui/material";
+import React from "react";
+
+
+export const KartuKeluargaList = () => {
+    const { dataGridProps } = useDataGrid();
+
+    console.log("dataGridProps:", dataGridProps); // Debugging
+
+    // Ambil data dari dataGridProps.rows.data
+    const rows = dataGridProps.rows?.data || [];
+    const total = dataGridProps.rows?.total || 0;
+
+
+    const columns = React.useMemo<GridColDef[]>(() => [
+        {
+            field: "nama_kepala_keluarga",
+            headerName: "Nama Kepala Keluarga",
+            flex: 1,
+            align: "center",
+            headerAlign: "center",
+        },
+
+        {
+            field: "alamat_keluarga",
+            headerName: "Alamat Kartu Keluarga",
+            flex: 1,
+            align: "center",
+            headerAlign: "center",
+        },
+
+
+        {
+            field: "nomor_telepon_keluarga",
+            headerName: "Nomor Telepon Keluarga",
+            flex: 1,
+            align: "center",
+            headerAlign: "center",
+        },
+
+        {
+          field: "actions",
+          headerName: "Actions",
+          flex: 1,
+          align: "center",
+          headerAlign: "center",
+          renderCell: (params) => (
+            <Box display="flex" justifyContent="center">
+              <EditButton recordItemId={params.row.id} />
+              <ShowButton recordItemId={params.row.id} />
+              <DeleteButton recordItemId={params.row.id} />
+            </Box>
+          ),
+        }
+
+    
+    ], []);
+
+    // return <MuiInferencer />;
+
+    return(
+    <List title="Kartu Keluarga">
+      <DataGrid
+        rows={rows} // Gunakan data dari rows
+        rowCount={total} // Gunakan total dari dataGridProps.rows.total
+        columns={columns}
+        autoHeight
+        pagination
+        paginationMode="server" // Sesuaikan dengan mode pagination backend
+        sortingMode="server" // Sesuaikan dengan mode sorting backend
+      />
+    </List>
+    )
+};
