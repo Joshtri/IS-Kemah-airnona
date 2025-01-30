deec102c93926cb02253df39d88cc3a4624ddde5
Joshtri <stuffofyos1516@gmail.com>
2025-01-30 23:31:54 +0800

    Refactor authentication provider to integrate Auth0 and streamline login/logout handling

--- src/providers/auth-provider.tsx
+++ src/providers/auth-provider.tsx
@@ -1,51 +1,41 @@
-// src/providers/auth-provider.ts
+import axios from "axios";
 import { AuthBindings } from "@refinedev/core";
+import { useAuth0 } from "@auth0/auth0-react";
 
-export const authProvider: AuthBindings = {
-    onError: async (error) => {
-        console.error("AuthProvider Error:", error);
-        return { 
-            success: false,
-            error 
-        };
-    },
-    login: async ({ username, password }) => {
-        // Simulasi API request untuk login
-        if (username === "admin" && password === "admin") {
-            localStorage.setItem("auth", JSON.stringify({ username }));
-            return {
-                success: true,
-                redirectTo: "/",
-            };
-        }
-        return {
-            success: false,
-            error: {
-                name: "LoginError",
-                message: "Invalid credentials",
-            },
-        };
+export const useAuthProvider = (): AuthBindings => {
+  const { isAuthenticated, user, logout, getIdTokenClaims } = useAuth0();
+
+  const authProvider: AuthBindings = {
+    login: async () => {
+      return { success: true };
     },
     logout: async () => {
-        localStorage.removeItem("auth");
-        return {
-            success: true,
-            redirectTo: "/login",
-        };
+      logout({ returnTo: window.location.origin });
+      return { success: true };
     },
     check: async () => {
-        const auth = localStorage.getItem("auth");
-        if (auth) {
-            return { authenticated: true };
-        }
-        return { authenticated: false, redirectTo: "/login" };
+      const token = await getIdTokenClaims();
+      if (isAuthenticated && token) {
+        axios.defaults.headers.common["Authorization"] = `Bearer ${token.__raw}`;
+        return { authenticated: true };
+      }
+      return {
+        authenticated: false,
+        redirectTo: "/login",
+        logout: true,
+      };
     },
     getPermissions: async () => null,
     getIdentity: async () => {
-        const auth = localStorage.getItem("auth");
-        if (auth) {
-            return JSON.parse(auth);
+        if (user) {
+          return {
+            ...user,
+            avatar: user.picture,
+          };
         }
         return null;
-    },
+      },
+  };
+
+  return authProvider;
 };
